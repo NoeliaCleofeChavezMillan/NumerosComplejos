a92c54dc4f323250f798f4962e404753d0821870
NoeliaCleofeChavezMillan <71261229@continental.edu.pe>
2021-12-08 14:49:37 -0500

    Implemnetacion de la funcionalidad HU001 SUMA

---
+++ tests/ComplejosTestCase.py
@@ -0,0 +1,12 @@
+import src.logica.libreria as lc
+import unittest
+
+class TestCplxoperations(unittest.TestCase):
+
+    def test(self):
+        suma = lc.cplxsum((3, 5),(-2.6, 6.8))
+        self.assertAlmostEqual(suma[0], 0.4)
+        self.assertAlmostEqual(suma[1], 11.8)
+
+
+
